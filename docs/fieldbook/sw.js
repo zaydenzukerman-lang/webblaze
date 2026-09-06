86a0253cfbd14f25ac7d103cbf53bdf5ccfa1548
Zayden Zukerman <zaydenzukerman@gmail.com>
2026-09-06 18:52:40 -0400

    Fieldbook: weekly goal ring, share-a-walk card, 4 new hunts (beach/fall/night/micro)

--- docs/fieldbook/sw.js
+++ docs/fieldbook/sw.js
@@ -1,4 +1,4 @@
-const C="fieldbook-v4";
+const C="fieldbook-v5";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
